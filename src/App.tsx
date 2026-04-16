e87cd4cd689b6812b880a1254ff6d5ce8c6c341c
Benciel Cabutin <cabutinb@gmail.com>
2026-04-16 18:41:19 +0800

    Starter files

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-4xl">
+        Survey form built using React + Typescript + Tailwindcss
+      </h1>
+    </div>
+  );
+}
+
+export default App;
